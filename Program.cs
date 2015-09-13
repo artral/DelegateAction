1fa2bed9d79fe5f6aaba2f017337c351e67a2b67
Artral <artralarasu@gmail.com>
2015-09-13 12:05:25 -0500

    Generic and Actions
    Simple example for Generics and Actions

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace DelegateAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string filename = @"c:\temp\test.txt";
+            //Create your own entity and use it as needed this is just a sample for string...
+            List<string> Result = EntityMaps.getObjectFromStore<string>(filename,
+                 (CmdParam) =>
+                 {
+                     CmdParam.CreateParameter().ParameterName = "@addurparam";
+                 }
+                 , Program.ReadFromFile);
+        }
+
+        public static string ReadFromFile(StreamReader fileReader)
+        {
+            string filename = @"c:\temp\test.txt";
+            fileReader = new StreamReader(filename);
+            return fileReader.ReadLine();
+        }
+    }
+}
+
